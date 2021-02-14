d68a6f10c77a400370bf4fbe13105c4045798608
Stefan Teunissen <stefanteunissen1@gmail.com>
2021-02-14 16:09:24 +0100

    Help page and set refresh period with passed argument

--- src/main/java/dev/stefanteunissen/cryptocurrentpricechecker/App.java
+++ src/main/java/dev/stefanteunissen/cryptocurrentpricechecker/App.java
@@ -6,11 +6,13 @@ import java.util.TimerTask;
 
 public class App {
     public static final int DELAY = 0;
-    public static final int PERIOD = 30000;
+    public static int PERIOD = 30000;
     public static final int ROUNDING_SCALE = 8;
     public static final RoundingMode ROUNDING_MODE = RoundingMode.HALF_EVEN;
 
     public static void main(String[] args) {
+        if (handleArguments(args))
+            return;
         PriceChecker priceChecker = new PriceChecker();
 
         Timer t = new Timer();
@@ -24,10 +26,30 @@ public class App {
                     clearConsole();
                     priceChecker.print();
                 } catch (Exception e) {
-                    System.out.println("Exception: "+e);
+                    System.out.println("Exception: " + e);
                 }
             }
         }, DELAY, PERIOD);
+    }
+
+    public static boolean handleArguments(String[] args) {
+        for (String arg : args) {
+            if (arg.equals("--help") || arg.equals("-h")) {
+                printHelp();
+                return true;
+            } else if (arg.startsWith("--period=") || arg.startsWith("-p=")) {
+                PERIOD = Integer.parseInt(arg.split("=")[1])*1000;
+            }
+        }
+        return false;
+    }
+
+    public static void printHelp() {
+        String commandFormat = "  %-20s%s%n";
+        System.out.printf(commandFormat, "-p=<seconds>","");
+        System.out.printf(commandFormat, "--period=<seconds>","How often the application should refresh");
+        System.out.printf(commandFormat, "-h","");
+        System.out.printf(commandFormat, "--help","Show this information");
     }
 
     public static void clearConsole() {
